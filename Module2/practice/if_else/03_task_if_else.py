70a80448621ae078f2a92e6b3cb14c3824254e14
RomanKapusta <88583751+RomanKapusta@users.noreply.github.com>
2021-08-07 16:05:00 +0300

    Update 03_task_if_else.py

--- Module2/practice/if_else/03_task_if_else.py
+++ Module2/practice/if_else/03_task_if_else.py
@@ -3,3 +3,12 @@
 # Формат выходных данных: Выведите «YES», если треугольник равнобедренный, и «NO» в противном случае.
 
 # TODO: your code here
+
+number_a = int(input("a ="))
+number_b = int(input("b ="))
+number_c = int(input("c ="))
+
+if number_a == number_b and number_b == number_c and number_c == number_a:
+    print ("Yes",)
+else:
+    print ("No",)
